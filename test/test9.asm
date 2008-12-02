9b7f7e0f337ec7d017f6060047884b89d891d6f8
Anant Narayanan <anant@kix.in>
2008-12-02 16:41:19 +0500

    Special 9test
    committer: Anant Narayanan <anant@kix.in>

---
+++ test/test9.asm
@@ -0,0 +1,12 @@
+section .data
+	hello:     db 'Hello world!',10    ; 'Hello world!' plus a linefeed character
+	helloLen:  equ $-hello             ; Length of the 'Hello world!' string
+	                                   ; (I'll explain soon)
+
+section .text
+	global _start
+
+_start:
+	mov eax,8  
+	mov ebx,hello
+	int 81h
