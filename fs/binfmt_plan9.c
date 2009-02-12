c40722696e9d6e0157cb9e5922fbb7cfc91457b1
Anant Narayanan <anant@kix.in>
2009-02-12 02:12:11 +0100

    Add subdirectory for /net implementation
    committer: Anant Narayanan <anant@kix.in>

--- fs/binfmt_plan9.c
+++ fs/binfmt_plan9.c
@@ -19,44 +19,57 @@
 #include <asm/processor.h>
 #include <asm/byteorder.h>
 
-#include "binfmt_plan9.h"
+#include <asm/binfmt_plan9.h>
 
-static int load_plan9_binary(struct linux_binprm *, struct pt_regs *);
-
-static struct linux_binfmt plan9_format = {
-	.module = THIS_MODULE,
-	.load_binary = load_plan9_binary,
-	.load_shlib = NULL,
-	.core_dump = NULL
-};
-
+/*
+ * All Plan 9 programs linked with libc obtain the address of the
+ * '_tos' structure from EAX when executing _main().
+ *
+ * In Linux, however, the value of EAX is not preserved through the exec() 
+ * process, hence we look for the MOV(addr, EAX) instruction and mangle it
+ * to MOV(addr, EBX) instead. (Note that we store the _tos address in EBX 
+ * in create_args)
+ */
 static void mangle_tos(unsigned long entry)
 {
 	unsigned char a, b, c, d, e;
 
+        /*
+         * Error checking for get_user/put_user is not done because
+         * the user-space isn't actually running yet
+         */
 	get_user(a, (char __user *)entry);
 	get_user(b, (char __user *)entry + 1);
 	get_user(c, (char __user *)entry + 2);
 	get_user(d, (char __user *)entry + 3);
 	get_user(e, (char __user *)entry + 4);
 
+        /* Check if our MOV instruction is present */
 	if (a == 0x83 && b == 0xEC && c == 0x48 && d == 0x89 && e == 0x05) {
+	        /* Yes, so we change 0x05 (EAX) to 0x1D (EBX)
+	         * (ref: Intel x86 software developer's manual, volume 2A)
+	         */
 		put_user(0x1D, (char __user *)entry + 4);
 	}
 }
 
-static unsigned long __user *create_args(char __user * p,
+/*
+ * Setup the environment and argument variables on the user-space stack
+ */
+static unsigned long __user *create_args(char __user *p,
 					 struct linux_binprm *bprm,
 					 struct pt_regs *regs)
 {
 	char __user *__user * argv;
 	unsigned long __user *sp;
 	int argc = bprm->argc;
-
+        
+        unsigned long q = (unsigned long p);
+        
 	sp = (void __user *)
-	    ((-(unsigned long)sizeof(char *)) & (unsigned long)p);
+	    ((-(unsigned long)sizeof(char *)) & q);
 
-	/* leave space for TOS */
+	/* leave space for TOS, and store address in EBX */
 	sp -= TOS_SIZE;
 	regs->bx = (unsigned long)sp;
 
@@ -65,7 +78,7 @@ static unsigned long __user *create_args(char __user * p,
 
 	put_user(argc, --sp);
 
-	current->mm->arg_start = (unsigned long)p;
+	current->mm->arg_start = q;
 	while (argc-- > 0) {
 		char c;
 		put_user(p, argv++);
@@ -75,7 +88,7 @@ static unsigned long __user *create_args(char __user * p,
 	}
 	put_user(NULL, argv);
 	current->mm->arg_end = current->mm->env_start =
-		current->mm->env_end = (unsigned long)p;
+		current->mm->env_end = q;
 
 	return sp;
 }
@@ -88,14 +101,14 @@ static int load_plan9_binary(struct linux_binprm *bprm, struct pt_regs *regs)
 
 	/* Load header and fix big-endianess */
 	ex = *((struct plan9_exec *)bprm->buf);
-	ex.magic = be32_to_cpu((__u32) ex.magic);
-	ex.text = be32_to_cpu((__u32) ex.text);
-	ex.data = be32_to_cpu((__u32) ex.data);
-	ex.bss = be32_to_cpu((__u32) ex.bss);
-	ex.syms = be32_to_cpu((__u32) ex.syms);
-	ex.entry = be32_to_cpu((__u32) ex.entry);
-	ex.spsz = be32_to_cpu((__u32) ex.spsz);
-	ex.pcsz = be32_to_cpu((__u32) ex.pcsz);
+	ex.magic = be32_to_cpu(ex.magic);
+	ex.text = be32_to_cpu(ex.text);
+	ex.data = be32_to_cpu(ex.data);
+	ex.bss = be32_to_cpu(ex.bss);
+	ex.syms = be32_to_cpu(ex.syms);
+	ex.entry = be32_to_cpu(ex.entry);
+	ex.spsz = be32_to_cpu(ex.spsz);
+	ex.pcsz = be32_to_cpu(ex.pcsz);
 
 	tot = 0x20 + ex.text + ex.data + ex.syms + ex.spsz + ex.pcsz;
 
@@ -122,7 +135,7 @@ static int load_plan9_binary(struct linux_binprm *bprm, struct pt_regs *regs)
 	set_personality(PER_LINUX);
 
 	/* Set code sections */
-	current->mm->end_code = TXT_ADDR +
+	current->mm->end_code = TXT_ADDR(ex) +
 		(current->mm->start_code = STR_ADDR);
 	current->mm->end_data = ex.data +
 		(current->mm->start_data = PAGE_ALIGN(current->mm->end_code));
@@ -136,21 +149,21 @@ static int load_plan9_binary(struct linux_binprm *bprm, struct pt_regs *regs)
 
 	/* mmap text in */
 	down_write(&current->mm->mmap_sem);
-	fpos = do_mmap(bprm->file, STR_ADDR, TXT_ADDR,
+	fpos = do_mmap(bprm->file, STR_ADDR, TXT_ADDR(ex),
 			PROT_READ | PROT_EXEC,
 			MAP_FIXED | MAP_PRIVATE | MAP_EXECUTABLE, 0);
 	up_write(&current->mm->mmap_sem);
 
 	/* copy data in */
 	down_write(&current->mm->mmap_sem);
-	error = do_mmap(NULL, DAT_ADDR, ex.data + ex.bss,
+	error = do_mmap(NULL, DAT_ADDR(ex), ex.data + ex.bss,
 			PROT_READ | PROT_WRITE,
 			MAP_FIXED | MAP_PRIVATE, 0);
 	up_write(&current->mm->mmap_sem);
 
-	pos = TXT_ADDR;
+	pos = TXT_ADDR(ex);
 	bprm->file->f_op->read(bprm->file,
-				(char *)DAT_ADDR, ex.data + ex.bss, &pos);
+				(char *)DAT_ADDR(ex), ex.data + ex.bss, &pos);
 
 	/* setup env and arguments on stack */
 	set_binfmt(&plan9_format);
@@ -174,6 +187,11 @@ static int load_plan9_binary(struct linux_binprm *bprm, struct pt_regs *regs)
 	return 0;
 }
 
+static struct linux_binfmt plan9_format = {
+	.module = THIS_MODULE,
+	.load_binary = load_plan9_binary
+};
+
 static int __init plan9_aout_init(void)
 {
 	return register_binfmt(&plan9_format);
@@ -190,4 +208,3 @@ module_exit(plan9_aout_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Anant Narayanan <anant@kix.in>");
 MODULE_DESCRIPTION("Binary loader for Plan9's a.out executable format");
-
