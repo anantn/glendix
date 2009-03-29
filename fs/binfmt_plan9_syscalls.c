17c2106e7e0eb596d25e7c508e68ce7d8307404e
Anant Narayanan <anant@kix.in>
2009-03-29 03:05:34 +0200

    Use more friendly argument names

--- fs/binfmt_plan9_syscalls.c
+++ fs/binfmt_plan9_syscalls.c
@@ -38,24 +38,24 @@ asmlinkage long sys_plan9_exits(struct pt_regs regs)
 
 asmlinkage long sys_plan9_chdir(struct pt_regs regs)
 {
-	unsigned long arg1;
+	unsigned long dirname;
 	unsigned long *addr = (unsigned long *)regs.esp;
 	printk(KERN_INFO "P9: Syscall %ld chdir called!\n", regs.eax);
 
-	get_user(arg1, ++addr);
+	get_user(dirname, ++addr);
 	
-	return sys_chdir((char __user *)arg1);
+	return sys_chdir((char __user *)dirname);
 }
 
 asmlinkage long sys_plan9_close(struct pt_regs regs)
 {
-	unsigned long arg1;
+	unsigned long fd;
 	unsigned long *addr = (unsigned long *)regs.esp;
 	printk(KERN_INFO "P9: Syscall %ld close called!\n", regs.eax);
 
-	get_user(arg1, ++addr);
+	get_user(fd, ++addr);
 	
-	return sys_close(arg1);
+	return sys_close(fd);
 }
 
 asmlinkage long sys_plan9_dup(struct pt_regs regs)
@@ -81,37 +81,35 @@ asmlinkage long sys_plan9_dup(struct pt_regs regs)
 
 asmlinkage long sys_plan9_open(struct pt_regs regs)
 {
-	unsigned long arg1, arg2;
+	unsigned long file, omode;
 	unsigned long *addr = (unsigned long *)regs.esp;
 	printk(KERN_INFO "P9: Syscall %ld open called!\n", regs.eax);
 
-	get_user(arg1, ++addr);
-	get_user(arg2, ++addr);
+	get_user(file, ++addr);
+	get_user(omode, ++addr);
 
 	/* FIXME: Mode needs to be check in all combos! */
-	return sys_open((const char __user *)arg1, arg2, (int) NULL);
+	return sys_open((const char __user *)file, omode, (int) NULL);
 }
 
 asmlinkage long sys_plan9_sleep(struct pt_regs regs)
 {
 	int rval;
-	unsigned long arg1;
 	struct timespec time;
-	
+	unsigned long millisecs;
 	unsigned long *addr = (unsigned long *)regs.esp;
 	printk(KERN_INFO "P9: Syscall %ld sleep called!\n", regs.eax);
 	
-	get_user(arg1, ++addr);
+	get_user(millisecs, ++addr);
 	
 	/* Milliseconds to seconds */
-	time.tv_sec = (time_t)arg1 / 1000;
-	arg1 -= time.tv_sec * 1000;
-	
+	time.tv_sec = (time_t)millisecs / 1000;
+	millisecs -= time.tv_sec * 1000;
 	/* Milliseconds to nanoseconds */
-	time.tv_nsec = arg1 * 1000000;
+	time.tv_nsec = millisecs * 1000000;
 	
 	printk(KERN_INFO "P9: sleep: %ldms resolved to %lds and %ldns\n",
-				arg1, time.tv_sec, time.tv_nsec);
+				millisecs, time.tv_sec, time.tv_nsec);
 	rval = sys_nanosleep(&time, &time);
 	
 	if (rval == 0)
@@ -122,16 +120,16 @@ asmlinkage long sys_plan9_sleep(struct pt_regs regs)
 
 asmlinkage long sys_plan9_create(struct pt_regs regs)
 {
-	unsigned long arg1, arg2, arg3;
+	unsigned long file, omode, perm;
 	unsigned long *addr = (unsigned long *)regs.esp;
 	printk(KERN_INFO "P9: Syscall %ld create called!\n", regs.eax);
 	
-	get_user(arg1, ++addr);
-	get_user(arg2, ++addr);
-	get_user(arg3, ++addr);
+	get_user(file, ++addr);
+	get_user(omode, ++addr);
+	get_user(perm, ++addr);
 	
-	/* TODO: check mode */
-	return sys_open((const char __user *)arg1, arg2 | O_CREAT, arg3);
+	/* TODO: check modes */
+	return sys_open((const char __user *)file, omode | O_CREAT, perm);
 }
 
 /* Original code is (C) Alexander Viro, linux-kernel, 12th Aug 2000
@@ -147,20 +145,19 @@ asmlinkage long sys_plan9_fd2path(struct pt_regs regs)
 	struct vfsmount *mnt;
 	struct dentry *dentry;
 	
-	unsigned long ba, size, len;
+	unsigned long abuf, nbuf, len;
+	char *page = (char *) __get_free_page(GFP_USER);
 	unsigned long *addr = (unsigned long *)regs.esp;
-	
 	printk(KERN_INFO "P9: Syscall %ld fd2path called!\n", regs.eax);
 	
-	char *page = (char *) __get_free_page(GFP_USER);
 	if (!page)
 		return -ENOMEM;
 	
 	get_user(fd, ++addr);
-	get_user(ba, ++addr);
-	get_user(size, ++addr);
+	get_user(abuf, ++addr);
+	get_user(nbuf, ++addr);
 
-	buf = (char __user *)ba;
+	buf = (char __user *)abuf;
 	
 	file = fget(fd);
 	if (!file)
@@ -173,7 +170,7 @@ asmlinkage long sys_plan9_fd2path(struct pt_regs regs)
 	cwd = d_path(dentry, mnt, page, PAGE_SIZE);
 	error = -ERANGE;
 	len = PAGE_SIZE + page - cwd;
-	if (len <= size) {
+	if (len <= nbuf) {
 		error = len;
 		if (copy_to_user(buf, cwd, len))
 			error = -EFAULT;
@@ -187,85 +184,89 @@ asmlinkage long sys_plan9_fd2path(struct pt_regs regs)
 	return error;
 }
 
+/* FIXME: Find out if this is brk_ or sbrk! */
 asmlinkage long sys_plan9_brk(struct pt_regs regs)
 {
-	unsigned long arg1;
+	unsigned long incr;
 	unsigned long *addr = (unsigned long *) regs.esp;
 	printk(KERN_INFO "P9: Syscall %ld brk called!\n", regs.eax);
 
-	get_user(arg1, ++addr);
+	get_user(incr, ++addr);
 	
-	return sys_brk(arg1);
+	return sys_brk(incr);
 }
 
 asmlinkage long sys_plan9_remove(struct pt_regs regs)
 {
-	unsigned long arg1;
+	unsigned long file;
 	unsigned long *addr = (unsigned long *) regs.esp;
 	printk(KERN_INFO "P9: Syscall %ld remove called!\n", regs.eax);
 
-	get_user(arg1, ++addr);
+	get_user(file, ++addr);
 
-	return sys_unlink((const char __user *)arg1);
+	return sys_unlink((const char __user *)file);
 }
 
+/* FIXME: Plan9's seek uses vlong, we ignore n here! */
 asmlinkage long sys_plan9_seek(struct pt_regs regs)
 {
 	loff_t offset;
-	unsigned long arg1, arg2, arg3;
+	unsigned long fd, n, type;
 	unsigned long *addr = (unsigned long *)regs.esp;
 	printk(KERN_INFO "P9: Syscall %ld seek called!\n", regs.eax);
 
-	get_user(arg1, ++addr);
-	get_user(arg2, ++addr);
+	get_user(n, ++addr);
+	get_user(fd, ++addr);
 	addr = addr + 2;
 	copy_from_user(&offset, addr, sizeof(loff_t));
-	get_user(arg3, ++addr);
+	get_user(type, ++addr);
 
-	return sys_lseek(arg2, (off_t) offset, arg3);
+	return sys_lseek(fd, (off_t) offset, type);
 }
 
 asmlinkage long sys_plan9_pread(struct pt_regs regs)
 {
 	loff_t offset;
-	unsigned long arg1, arg2, arg3;
+	unsigned long fd, buf, nbytes;
 	unsigned long *addr = (unsigned long *)regs.esp;
 	printk(KERN_INFO "P9: Syscall %ld pread called!\n", regs.eax);
 
-	get_user(arg1, ++addr);
-	get_user(arg2, ++addr);
-	get_user(arg3, ++addr);
+	get_user(fd, ++addr);
+	get_user(buf, ++addr);
+	get_user(nbytes, ++addr);
 	addr = addr + 2;
 	copy_from_user(&offset, addr, sizeof(loff_t));
 
 	printk(KERN_INFO "P9: pread: offset: %llx\n", offset);
 	if (offset == 0xffffffff) {
-		printk(KERN_INFO "P9: pread: calling with %lx, %lx, %lx\n", arg1, arg2, arg3);
-		return sys_read(arg1, (char __user *)arg2, arg3);
+		printk(KERN_INFO "P9: pread: calling with %lx, %lx, %lx\n",
+							fd, buf, nbytes);
+		return sys_read(fd, (char __user *)buf, nbytes);
 	} else {
-		return sys_pread64(arg1, (char __user *)arg2, arg3, offset); 
+		return sys_pread64(fd, (char __user *)buf, nbytes, offset); 
 	}
 }
 
 asmlinkage long sys_plan9_pwrite(struct pt_regs regs)
 {
 	loff_t offset;
-	unsigned long arg1, arg2, arg3;
+	unsigned long fd, buf, nbytes;
 	unsigned long *addr = (unsigned long *)regs.esp;
 	printk(KERN_INFO "P9: Syscall %ld pwrite called!\n", regs.eax);
 
-	get_user(arg1, ++addr);
-	get_user(arg2, ++addr);
-	get_user(arg3, ++addr);
+	get_user(fd, ++addr);
+	get_user(buf, ++addr);
+	get_user(nbytes, ++addr);
 	addr = addr + 2;
 	copy_from_user(&offset, addr, sizeof(loff_t));
 
 	printk(KERN_INFO "P9: pwrite: offset: %llx\n", offset);
 	if (offset == 0xffffffff) {
-		printk(KERN_INFO "P9: pwrite: calling with %lx, %lx, %lx\n", arg1, arg2, arg3);
-		return sys_write(arg1, (char __user *)arg2, arg3);
+		printk(KERN_INFO "P9: pwrite: calling with %lx, %lx, %lx\n",
+							fd, buf, nbytes);
+		return sys_write(fd, (char __user *)buf, nbytes);
 	} else {
-		return sys_pwrite64(arg1, (char __user *)arg2, arg3, offset);
+		return sys_pwrite64(fd, (char __user *)buf, nbytes, offset);
 	}
 }
 
